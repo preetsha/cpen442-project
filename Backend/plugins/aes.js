56ed2e66d9af2d72e52248c1bca57fe3e78ee6a7
thomaslapadat <thomaslapadat@gmail.com>
2021-11-27 15:03:37 -0800

    Remove ignored files

--- Backend/plugins/aes.js
+++ Backend/plugins/aes.js
@@ -2,8 +2,8 @@ const crypto = require("crypto");
 
 module.exports = {
     encrypt: (plaintext, ivString, keyString) => {
-        if (plaintext.length < 16) 
-            throw `Plaintext length ${plaintext.length} is too short!`;
+        // if (plaintext.length < 16) 
+        //     throw `Plaintext length ${plaintext.length} is too short!`;
 
         if (ivString.length != 16) 
             throw `IV string length ${ivString.length} is invalid`;
@@ -16,12 +16,12 @@ module.exports = {
         const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
         const encrypted = cipher.update(plaintext, "utf-8", "hex");
 
-        return encrypted;
+        return encrypted + cipher.final("hex");
     },
 
     decrypt: (ciphertext, ivString, keyString) => {
-        if (ciphertext.length < 16) 
-            throw `Plaintext length ${ciphertext.length} is too short!`;
+        // if (ciphertext.length < 16) 
+        //     throw `Plaintext length ${ciphertext.length} is too short!`;
 
         if (ivString.length != 16) 
             throw `IV string length ${ivString.length} is invalid`;
@@ -32,8 +32,43 @@ module.exports = {
         const iv = Buffer.from(ivString, "utf-8");
         const key = Buffer.from(keyString, "utf-8");
         const cipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-        const decrypted = cipher.update(ciphertext, "utf-8", "hex");
+        const decrypted = cipher.update(ciphertext, "hex", "utf-8");
 
-        return decrypted;
+        return decrypted + cipher.final("hex");
     }
+    // encrypt: (plaintext, ivString, keyString) => {
+    //     if (plaintext.length < 16) 
+    //         throw `Plaintext length ${plaintext.length} is too short!`;
+
+    //     if (ivString.length != 16) 
+    //         throw `IV string length ${ivString.length} is invalid`;
+
+    //     if (keyString.length != 32) 
+    //         throw `Keystring length ${keyString.length} is invalid`;
+        
+    //     const iv = Buffer.from(ivString, "utf-8");
+    //     const key = Buffer.from(keyString, "utf-8");
+    //     const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+    //     const encrypted = cipher.update(plaintext, "utf-8", "hex");
+
+    //     return encrypted;
+    // },
+
+    // decrypt: (ciphertext, ivString, keyString) => {
+    //     if (ciphertext.length < 16) 
+    //         throw `Plaintext length ${ciphertext.length} is too short!`;
+
+    //     if (ivString.length != 16) 
+    //         throw `IV string length ${ivString.length} is invalid`;
+
+    //     if (keyString.length != 32) 
+    //         throw `Keystring length ${keyString.length} is invalid`;
+        
+    //     const iv = Buffer.from(ivString, "utf-8");
+    //     const key = Buffer.from(keyString, "utf-8");
+    //     const cipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+    //     const decrypted = cipher.update(ciphertext, "utf-8", "hex");
+
+    //     return decrypted;
+    // }
 }
