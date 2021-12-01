c69a1a8df9cf87f4493edd370efe6f2ce0a1b0e3
Odin <38874722+mebesius@users.noreply.github.com>
2021-12-01 10:32:30 -0800

    initial implementation

--- Frontend/app/src/main/java/com/example/myapplication/SplashActivity.java
+++ Frontend/app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -7,10 +7,21 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import java.math.BigInteger;
+import java.util.Random;
 
 public class SplashActivity extends Activity {
 
@@ -60,14 +71,21 @@ public class SplashActivity extends Activity {
             }
         }
 
-        SharedPreferences pref = this.getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE);
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE);
         Intent registrationIntent = new Intent(SplashActivity.this, RegistrationActivity.class);
         if (SMSContacts.isInternetAvailable()) {
             if (pref.getString("UUID", "").isEmpty() || pref.getString("SECRET", "").isEmpty()) {
                 startActivity(registrationIntent);
             } else {
                 //TODO: call backend enpoint and use existing session key
-                generateSessionKey();
+                /*
+                if (sk exists in cache):
+                    f() which calls Thomas's endpoint to check if
+                    can continue
+                 else:
+                    generateSessionKey(); // update session key in cache
+                 */
+                generateSessionKey(pref, getApplicationContext());
                 finish();
             }
         } else {
@@ -84,10 +102,130 @@ public class SplashActivity extends Activity {
         finish();
     }
 
-    private void generateSessionKey() {
+    private void generateSessionKey(SharedPreferences preferences, Context context) {
         // TODO: Generate new session key
-        SMSContacts.setContactList(SMSContacts.populateSMSGroups(getApplicationContext()));
-        Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
-        startActivity(mainIntent);
+        boolean isKeyEstablished = initGetSessionKey(preferences, context);
+        if (isKeyEstablished) {
+            SMSContacts.setContactList(SMSContacts.populateSMSGroups(getApplicationContext()));
+            Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(mainIntent);
+        } else {
+            Log.e("generateSessionKey", "Could not establish session key");
+            Toast.makeText(
+                    context,
+                    "Failed to establish a session key, please try again later",
+                    Toast.LENGTH_LONG
+            ).show();
+        }
+    }
+
+    private boolean initGetSessionKey(SharedPreferences preferences, Context context) {
+        final boolean[] successful = {false};
+
+        String root = "http://ec2-54-241-2-134.us-west-1.compute.amazonaws.com:8080";
+        String route = "/user/trust";
+        String url = root + route;
+        BigInteger g = new BigInteger("5");
+        BigInteger p = new BigInteger("23");;
+        int a = new Random(System.currentTimeMillis()).nextInt(p.intValue() - (2 * g.intValue())) + g.intValue();
+        BigInteger a2 = new BigInteger(Integer.toString(a));
+        String nonce = Integer.toString(new Random(System.currentTimeMillis()).nextInt());
+        String keyhalf = g.modPow(a2, p).toString();
+        JSONObject jsonBody = new JSONObject();
+        JSONObject payload = new JSONObject();
+        try {
+            payload.put("uuid", preferences.getString("UUID", ""));
+            payload.put("keyhalf", keyhalf);
+            jsonBody.put("uuid", preferences.getString("UUID", ""));
+            jsonBody.put("nonce", nonce);
+            jsonBody.put("payload", payload.toString());
+        } catch (Exception e) {
+            Log.e("generateSessionKey", "JSON body put error");
+        }
+        // Request a string response from the provided URL.
+        String requestBody = jsonBody.toString();
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, jsonBody,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        String responseNonce = response.getString("nonce");
+                        JSONObject responsePayload = response.getJSONObject("payload");
+                        String responseChallengeNonce = responsePayload.getString("nonce");
+                        String responseKeyhalf = responsePayload.getString("keyhalf");
+
+                        if (nonce.contentEquals(responseChallengeNonce)) {
+                            // make next request and store session key
+                            successful[0] = finGetSessionKey(responseKeyhalf, a2, p, preferences, responseNonce, url, context) || successful[0];
+                        } else {
+                            throw new Exception("Could not verify server identity");
+                        }
+                    } catch (Exception e) {
+                        Log.e("initGetSessionKey response", "JSON body get error", e);
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("generateSessionKey - initGetSessionKey", "onErrorResponse: ", error);
+                    Toast.makeText(
+                            context,
+                            "Failed to establish a session key, please try again later",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+            }
+        );
+
+        // Add the request to the RequestQueue.
+        QueueSingleton.getInstance(context).addToRequestQueue(jsonRequest);
+        return successful[0];
+    }
+
+    private boolean finGetSessionKey(String responseKeyhalf, BigInteger a2, BigInteger p,
+                                  SharedPreferences preferences, String responseNonce, String url,
+                                  Context context) {
+        // make json body
+        JSONObject jsonBody = new JSONObject();
+        JSONObject payload = new JSONObject();
+
+        final boolean[] successful = {false};
+
+        try {
+            payload.put("uuid", preferences.getString("UUID", ""));
+            payload.put("nonce", responseNonce);
+            jsonBody.put("uuid", preferences.getString("UUID", ""));
+            jsonBody.put("payload", payload);
+
+            JsonObjectRequest jsonRequestFin = new JsonObjectRequest(Request.Method.POST, url, jsonBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // store session key
+                        BigInteger gbmodp = new BigInteger(responseKeyhalf);
+                        String sessionKey = gbmodp.modPow(a2, p).toString();
+                        preferences.edit().putString("SESSION_KEY", sessionKey).apply();
+                        successful[0] = true;
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("generateSessionKey - finGetSessionKey", "onErrorResponse: ", error);
+                        Toast.makeText(
+                                context,
+                                "Failed to establish a session key, please try again later",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                }
+            );
+            // Add the request to the RequestQueue.
+            QueueSingleton.getInstance(context).addToRequestQueue(jsonRequestFin);
+        } catch (Exception e) {
+            Log.e("generateSessionKey - finGetSessionKey", "JSON body put error");
+        }
+
+        return successful[0];
     }
 }
